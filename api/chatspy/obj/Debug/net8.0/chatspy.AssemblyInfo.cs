c3f4f84077f24b0205caed17e24721cf5403d223
Boluwatife Ajibola <ajibolss11@gmail.com>
2024-08-29 13:43:04 -0400

    added the gitignore file

--- api/chatspy/obj/Debug/net8.0/chatspy.AssemblyInfo.cs
+++ api/chatspy/obj/Debug/net8.0/chatspy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chatspy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4df1717229cffdf5e4625e6001a5153ba85816")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cec32f44131c166ea00da370eab10804d3159443")]
 [assembly: System.Reflection.AssemblyProductAttribute("chatspy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chatspy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

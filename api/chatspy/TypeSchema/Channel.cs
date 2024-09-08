185e56cf451404363132b8f587cde5bce625878d
Boluwatife Ajibola <ajibolss11@gmail.com>
2024-09-07 22:41:02 -0400

    added the resulver for the User field in the Channel schema

--- api/chatspy/TypeSchema/Channel.cs
+++ api/chatspy/TypeSchema/Channel.cs
@@ -1,4 +1,7 @@
 using System;
+using chatspy.Data;
+using chatspy.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace chatspy.TypeSchema;
 
@@ -7,7 +10,39 @@ public class Channel
     public Guid Id { get; set; }
     public string Name { get; set; }
     public ChannelType Type { get; set; }
-    public List<User> PrivateUsers { get; set; }
+
+    public async Task<List<User>> Users(ChatspyContext dbContext)
+    {
+        List<User> users;
+        List<UserModel> dbUsers = null!;
+        var dbChannel = await dbContext.Channels.SingleAsync(c => c.Id == Id);
+
+        if (Type == ChannelType.Private)
+        {
+            dbUsers = dbChannel.Users.ToList();
+        }
+        else if (Type == ChannelType.Direct)
+        {
+            dbUsers = dbChannel.Users.ToList();
+        }
+        //Type == ChannelType.Public
+        else
+        {
+            dbUsers = dbChannel.Workspace.Users.ToList();
+        }
+
+        users = dbUsers
+            .Select(u => new User
+            {
+                Username = u.Username,
+                FullName = u.FullName,
+                Email = u.Email,
+                ProfilePicture = u.ProfilePicture,
+            })
+            .ToList();
+        return users;
+    }
+
     public List<Message> Messages { get; set; }
 }
 

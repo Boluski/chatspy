deadf3aa39a0fe5fca49df9e95f997119a11fdd5
Boluwatife Ajibola <ajibolss11@gmail.com>
2024-09-27 19:57:25 -0400

    added the WorkspaceHeader

---
+++ frontend/chatspy/src/app/components/workspaceHeader.tsx
@@ -0,0 +1,28 @@
+import { Group, Avatar, Title, TextInput, DEFAULT_THEME } from "@mantine/core";
+import { IoSearchSharp } from "react-icons/io5";
+
+export default function WorkspaceHeader() {
+  return (
+    <Group
+      style={{ borderBottom: `solid 2px ${DEFAULT_THEME.colors.dark[0]}` }}
+      py={5}
+    >
+      <Group>
+        <Avatar
+          name="Spellblaze"
+          size={"lg"}
+          radius={"lg"}
+          styles={{ placeholder: { fontSize: "2rem" } }}
+        />
+        <Title c={"dark.6"}>Spellblaze</Title>
+      </Group>
+      <TextInput
+        style={{ flexGrow: "1" }}
+        size="lg"
+        placeholder="Search Anything"
+        leftSection={<IoSearchSharp size={"1.7rem"} />}
+      />
+      <Avatar name="Boluwatife Ajibola" size={"lg"} />
+    </Group>
+  );
+}
--- frontend/chatspy/src/app/workspace/[workspaceID]/page.tsx
+++ frontend/chatspy/src/app/workspace/[workspaceID]/page.tsx
@@ -1,9 +1,83 @@
 "use Client";
+import {
+  Stack,
+  Avatar,
+  Title,
+  Button,
+  Group,
+  TextInput,
+  DEFAULT_THEME,
+  ActionIcon,
+} from "@mantine/core";
+import { IoSearchSharp } from "react-icons/io5";
+import { FaHashtag } from "react-icons/fa6";
+import { IoLockClosed } from "react-icons/io5";
+import { IoPersonSharp } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdAdd } from "react-icons/md";
+import WorkspaceHeader from "@/app/components/workspaceHeader";
 
 type Workspace = {
   params: { workspaceID: string };
 };
 
 export default function Workspace({ params }: Workspace) {
-  return <h1>Workspace: {params.workspaceID}</h1>;
+  return (
+    <Stack w={"100wh"} h={"100vh"} bg={"gray.0"} px={15} py={10}>
+      <WorkspaceHeader />
+      <Stack
+        w={"fit-content"}
+        h={"100%"}
+        justify={"space-between"}
+        style={{ borderRight: `solid 2px ${DEFAULT_THEME.colors.dark[0]}` }}
+        pr={"sm"}
+      >
+        <Stack>
+          <ActionIcon
+            color="gray"
+            variant="light"
+            size="xl"
+            style={{ width: "4rem", height: "4rem" }}
+          >
+            <FaHashtag size={"2.5rem"} />
+          </ActionIcon>
+          <ActionIcon
+            color="gray"
+            variant="light"
+            size="xl"
+            style={{ width: "4rem", height: "4rem" }}
+          >
+            <IoLockClosed size={"2.5rem"} />
+          </ActionIcon>
+          <ActionIcon
+            color="gray"
+            variant="light"
+            size="xl"
+            style={{ width: "4rem", height: "4rem" }}
+          >
+            <IoPersonSharp size={"2.5rem"} />
+          </ActionIcon>
+        </Stack>
+
+        <Stack>
+          <ActionIcon
+            color="violet.8"
+            variant="light"
+            size="xl"
+            style={{ width: "4rem", height: "4rem" }}
+          >
+            <MdAdd size={"2.5rem"} />
+          </ActionIcon>
+          <ActionIcon
+            color="violet.8"
+            variant="light"
+            size="xl"
+            style={{ width: "4rem", height: "4rem" }}
+          >
+            <IoSettingsOutline size={"2.5rem"} />
+          </ActionIcon>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
 }

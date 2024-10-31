a21f50ec54563f64d8f4c6bed93774fab9b9313f
Boluwatife Ajibola <ajibolss11@gmail.com>
2024-10-31 14:47:23 -0400

    frontend code clean up

--- src/app/ApolloWrapper.tsx
+++ src/app/ApolloWrapper.tsx
@@ -1,5 +1,4 @@
 "use client";
-// ^ this file needs the "use client" pragma
 
 import { split, HttpLink } from "@apollo/client";
 import {
@@ -11,7 +10,6 @@ import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 
-// have a function to create a client for you
 function makeClient() {
   const wsLink = new GraphQLWsLink(
     createClient({
@@ -20,15 +18,9 @@ function makeClient() {
   );
 
   const httpLink = new HttpLink({
-    // this needs to be an absolute url, as relative urls cannot be used in SSR
     uri: process.env.NEXT_PUBLIC_HTTP_URL as string,
-    // you can disable result caching here if you want to
-    // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+
     fetchOptions: { cache: "no-store" },
-    // you can override the default `fetchOptions` on a per query basis
-    // via the `context` property on the options passed as a second argument
-    // to an Apollo Client data fetching hook, e.g.:
-    // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
   });
 
   const splitLink = split(
@@ -43,15 +35,12 @@ function makeClient() {
     httpLink
   );
 
-  // use the `ApolloClient` from "@apollo/experimental-nextjs-app-support"
   return new ApolloClient({
-    // use the `InMemoryCache` from "@apollo/experimental-nextjs-app-support"
     cache: new InMemoryCache(),
     link: splitLink,
   });
 }
 
-// you need to create a component to wrap your app in
 export function ApolloWrapper({ children }: React.PropsWithChildren) {
   return (
     <ApolloNextAppProvider makeClient={makeClient}>

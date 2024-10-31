8d3e2505bb96b15aeb644b3890db74a9877997a5
Boluwatife Ajibola <ajibolss11@gmail.com>
2024-10-31 14:34:03 -0400

    api code clean up

--- api/chatspy/TypeSchema/Thread.cs
+++ api/chatspy/TypeSchema/Thread.cs
@@ -10,13 +10,12 @@ public class Thread
     public DateTime Date { get; set; }
 
     [GraphQLIgnore]
-    public Guid? MessageId { get; set; }
-
-    [GraphQLIgnore]
     public string? Username { get; set; }
 
     public async Task<User> User(ChatspyContext dbContext)
     {
+        // Specifically using the Username variable to get the user instead for Id
+        // So as to avoid the Sequence contains more than one element error that is thrown.
         var dbUser = await dbContext.Users.SingleAsync(u => u.Username == Username);
 
         var user = new User
